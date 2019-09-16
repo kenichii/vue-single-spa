dca20eca46c45c9ffd8e3bbd378bade2b82b605e
kenneth.reyes <kennethreyes.mvp@gmail.com>
2019-09-16 15:19:31 +0800

    Initial Commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import singleSpaVue from 'single-spa-vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const vueLifecycles = singleSpaVue({
+  Vue,
+  appOptions: {
+    render: (h: any) => h(App),
+    router,
+  },
+});
+
+export const bootstrap = vueLifecycles.bootstrap;
+export const mount = vueLifecycles.mount;
+export const unmount = vueLifecycles.unmount;
